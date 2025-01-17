59ac2c2a5bc78171ca5e193778e50fb63a608dae
amirhosein993 <amirhusainmaleki1996@gmail.com>
2025-01-17 14:47:27 +0330

    add get random color helper to appointment repository

--- src/app/calendar/calendar/service/appointment-repository.service.ts
+++ src/app/calendar/calendar/service/appointment-repository.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {AppointmentService} from './appointment.service';
-import {Observable} from 'rxjs';
+import {map, Observable} from 'rxjs';
 import {Appointment} from '../model/appointment';
+import {Helper} from '../../../../helpers/helper';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,13 @@ export class AppointmentRepositoryService {
   constructor(private appointmentsService : AppointmentService) { }
 
   public getAppointments() : Observable<Appointment[]> {
-    return this.appointmentsService.getAppointments();
+    return this.appointmentsService.getAppointments().pipe(map(appointments => {
+      return appointments.map(item => {
+        return {
+          ...item ,
+         color : Helper.getRandomColor()
+        }
+      })
+    }));
   }
 }
